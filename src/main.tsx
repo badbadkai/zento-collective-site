faff81a45407efe8e0b2c8200c370564e10b5af1
badbadkai <projectskyfall24@gmail.com>
2025-12-06 23:16:15 +0000

    Changed website title

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Ensure the browser tab shows the concise site title
+document.title = "Greenridge Studios";
+
 createRoot(document.getElementById("root")!).render(<App />);
